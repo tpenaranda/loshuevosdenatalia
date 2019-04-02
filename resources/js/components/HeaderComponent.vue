9253edc4aeee6d73f146e30946703236b9c8e2ab
Tate Peñaranda <tate@blockinit.io>
2019-04-02 13:18:11 -0300

    Bugfix, added 'moment' to HeaderComponent

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -2,12 +2,14 @@
     <md-card-header class="p-0">
         <div class="md-display-1 text-center p-2 text-light bg-primary">ARTESANALES Y PERSONALIZADOS</div>
         <div class="md-body-1 text-center pb-2 text-light bg-primary">
-            ¡Estamos a {{ $moment(easter_date).fromNow(true) }} de pascuas!
+            ¡Estamos a {{ days_from_easter_text }} de pascuas!
         </div>
     </md-card-header>
 </template>
 
 <script>
+    import moment from 'moment-timezone'
+
     export default {
         name: 'HeaderComponent',
         components: {
@@ -18,6 +20,9 @@
             easter_date: '2019-04-20',
         }),
         computed: {
+            days_from_easter_text () {
+                return moment(this.easter_date).locale('es').fromNow(true)
+            }
         },
         mounted () {
         },
